44ea2f900edef87c3cc04db1bffe7c2fb6d0c116
Sanghita Seal <sealsanghita2005@gmail.com>
2026-05-14 18:47:42 +0530

    Added server api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://live-polling-application-1816.onrender.com",
         changeOrigin: true,
         secure: false,
       },
